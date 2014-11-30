05e8c5560bf1927d4e128f47c2cead5759f3afda
Daniel Mané <danmane@gmail.com>
2014-11-30 00:10:58 -0800

    Move pairs to module namespace

--- pairs.ts
+++ pairs.ts
@@ -1,6 +1,10 @@
-export function pairs<T>(xs: T[]): [T,T][] {
-	function toPair(x: T, i: number): [T,T] {
-		return [x, xs[i+1]];
+module Abalone {
+export module Util {
+	export function pairs<T>(xs: T[]): [T,T][] {
+		function toPair(x: T, i: number): [T,T] {
+			return [x, xs[i+1]];
+		}
+		return xs.slice(0, xs.length-1).map(toPair);
 	}
-	return xs.slice(0, xs.length-1).map(toPair);
 }
+}	
